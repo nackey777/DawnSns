c3481dbe128c7d5b7e0c907cc806ecbc8973b586
LAPTOP-OOK3AUMS\alice <lemonglass765@gmail.com>
2022-08-19 22:03:15 +0900

    マイグレーションの日時設定を変更

--- database/migrations/2019_12_24_090209_create_posts_table.php
+++ database/migrations/2019_12_24_090209_create_posts_table.php
@@ -17,7 +17,8 @@ class CreatePostsTable extends Migration
             $table->increments('id')->autoIncrement();
             $table->integer('user_id');
             $table->string('post',500);
-            $table->timestamps();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->default(DB::raw("CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP"));
         });
     }
 
