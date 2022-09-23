8341353c18af68a6eccaca3a9324929cfe76ad68
LAPTOP-OOK3AUMS\alice <lemonglass765@gmail.com>
2022-09-23 12:59:46 +0900

    投稿フォームの設置

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.login')
-
 @section('content')
-<h2>機能を実装していきましょう。</h2>
+
+<form class="post">
+    <img class="post_face" src="images/dawn.png">
+    <textarea class="post_input" placeholder="何をつぶやこうか...?"></textarea>
+    <a href=""><img class="post_submit" src="images/post.png"></a>
+</form>
 
 @endsection
